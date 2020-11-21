420645ccbba3d01cac156f4e3130063660c3ff10
boni <x.bonillaa@gmail.com>
2020-11-20 22:11:28 -0500

    ....

--- js/app.js
+++ js/app.js
@@ -6,4 +6,25 @@ let tweets = [];
 
 // Event Listeners
 
+eventListeners();
+
+function eventListeners() {
+    formulario.addEventListener('submit', agregarTweet);
+} 
+
 // Funciones
+
+function agregarTweet(e) {
+    e.preventDefault();
+
+    // Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    // Validacion...
+    if(tweet === '') {
+        console.log('no puede ir vacio');
+
+        return; // Evita que se ejecuten mas lineas de codigo
+    }
+    console.log('fpjawjgpg');
+}
